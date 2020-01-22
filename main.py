4bcf86596181b52d210649f750b2f7757146aae3
Jacollier97 <Jacollierjc1997@gmail.com>
2020-01-21 21:01:29 -0800

    Compare Function

--- main.py
+++ main.py
@@ -15,6 +15,29 @@ def ask_user():
 def gen_opp():
     return random.choice(["r", "p", "s"])
 
+def compare(answer, opp):
+    if answer == 'r':
+        if opp == 'r':
+            return 'draw'
+        if opp == 'p':
+            return 'lose'
+        if opp == 's':
+            return 'win'
+    if answer == 'p':
+        if opp == 'r':
+            return 'win'
+        if opp == 'p':
+            return 'draw'
+        if opp == 's':
+            return 'lose'
+    if answer == 's':
+        if opp == 'r':
+            return 'lose'
+        if opp == 'p':
+            return 'win'
+        if opp == 's':
+            return 'draw'
+
 
 def main():
     answer = ask_user()
