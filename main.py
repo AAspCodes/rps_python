fcde832837b4f04588d5a690b06ed3e880a66eef
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-01-21 20:36:30 -0800

    improve gen_opp()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 def ask_user():
     prompt = "Rock, Paper, or Scissors? r/p/s: "
     while True:
@@ -10,19 +11,14 @@ def ask_user():
             print("That's not an option.")
     return answer
 
+
 def gen_opp():
-    opp = random.randint(0,2)
-    if opp == 0:
-        return "r"
-    if opp == 1:
-        return "p"
-    if opp == 2:
-        return "s"
+    return random.choice(["r", "p", "s"])
+
 
 def main():
     answer = ask_user()
     opp = gen_opp()
-    
 
 
 if __name__ == "__main__":
