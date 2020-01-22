624951b534671dc7c02743a7f098634001dfc32e
Jacollier97 <Jacollierjc1997@gmail.com>
2020-01-21 20:32:13 -0800

    New function
    Generate Opponent function
    
    Import Random

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def ask_user():
     prompt = "Rock, Paper, or Scissors? r/p/s: "
     while True:
@@ -8,9 +10,19 @@ def ask_user():
             print("That's not an option.")
     return answer
 
+def gen_opp():
+    opp = random.randint(0,2)
+    if opp == 0:
+        return "r"
+    if opp == 1:
+        return "p"
+    if opp == 2:
+        return "s"
 
 def main():
     answer = ask_user()
+    opp = gen_opp()
+    
 
 
 if __name__ == "__main__":
